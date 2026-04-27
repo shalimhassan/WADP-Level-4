948a5eb203d047ec3e12456a42bee6e00c129fe5
Shalim Hassan <salim.hassan219@gmail.com>
2026-04-28 03:24:56 +0600

    Update models.py

--- Day 16 - App Setup, Data Add from Frontend and Show/school_app/models.py
+++ Day 16 - App Setup, Data Add from Frontend and Show/school_app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class studentModel(models.Model):
-  name = models.CharField(max_length=100)
-  age = models.IntegerField()
-  roll_no = models.IntegerField()
 
+class studentModel(models.Model):
+  name = models.CharField(max_length=200)
+  address = models.TextField()
+  email = models.EmailField()
+  admission_date = models.DateField()
+  
   def __str__(self):
     return f'{self.name}'
